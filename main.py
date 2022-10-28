1bc962c6e6803ff78a74f3df0b321610e93fac6d
Josh <jpostman314@yahoo.com>
2022-10-28 12:44:02 -0400

    Created Lists of Choices

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello World!!")
+
+import random
+
+
+
+
+
+
+
+destinations = ["Buffalo", "Syracuse", "Albany", "Manhatten", "Boston", "Baltimore", "Myrtle Beach", "Miami", "Houston"]
+restaurants = ["Anchor Bar", "Pies Guys", "Carrabba's", "Minetta Tavern", "The Smith", "Carmine's", "Ms. Cheezious", "KOMODO", "Coyo Taco", "The Pit Room", "Pinkerton's Barbecue"]
+mode of transportation = ["Car", "Private Jet", "Walk", "Motorcycle", "Bicycle", "Limo", "Train", "Boat"]
+entertainment = ["Comedy Show", "Mall", "Parkour", "Museum", "Live Music", "Beach", "Rent Jet Skis", "Go-Karts", "Mini-Golf", "Park"]
+
+
+
