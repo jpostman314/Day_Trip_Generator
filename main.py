f63ec6c53d6705f373493ebb6bc3fe51defdf881
Josh <jpostman314@yahoo.com>
2022-10-31 23:20:15 -0400

    Changed Walk choice to Walking.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from select import select
 
 destinations = ["Buffalo", "Syracuse", "Albany", "Manhatten", "Boston", "Baltimore", "Myrtle Beach", "Miami", "Houston"]
 restaurants = ["Anchor Bar", "Pies Guys", "Carrabba's", "Minetta Tavern", "The Smith", "Carmine's", "Ms. Cheezious", "KOMODO", "Coyo Taco", "The Pit Room", "Pinkerton's Barbecue"]
-mode_of_transportation = ["Car", "Private Jet", "Walk", "Motorcycle", "Bicycle", "Limo", "Train", "Boat"]
+mode_of_transportation = ["Car", "Private Jet", "Walking", "Motorcycle", "Bicycle", "Limo", "Train", "Boat"]
 entertainment = ["Comedy Show", "Mall", "Parkour", "Museum", "Live Music", "Beach", "Rent Jet Skis", "Go-Karts", "Mini-Golf", "Park"]
 
 day_trip_generator = [destinations, restaurants, mode_of_transportation, entertainment]
